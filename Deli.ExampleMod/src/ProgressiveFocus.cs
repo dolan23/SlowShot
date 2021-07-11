43c5d85d9707c0ac7f3effc5338f71466126cfd0
dolan23 <vlady.vaisberg@gmail.com>
2021-07-11 08:21:49 +0200

    bla

---
+++ Deli.ExampleMod/src/ProgressiveFocus.cs
@@ -0,0 +1,21 @@
+﻿using Deli.Immediate;
+using Deli.Setup;
+using UnityEngine;
+using FistVR;
+
+namespace Deli.ProgressiveFocus
+{
+	public class ProgressiveFocus : DeliBehaviour
+	{
+		public ProgressiveFocus()
+		{
+			UnityEngine.Debug.Log("Loading ProgressiveFocus");
+			GM.CurrentMovementManager.Hands.
+		}
+
+		public void Update() {
+			UnityEngine.Debug.Log("FrameUpdate");
+		}
+
+	}
+}
